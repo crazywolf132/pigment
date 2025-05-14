7c7447cf2cb4766f91cc582fa5402de2f70b7000
crazywolf132 <crazywolf132@gmail.com>
2025-05-15 02:45:42 +1000

    Add usage examples

---
+++ examples/basic.rs
@@ -0,0 +1,17 @@
+use pigment::color;
+
+fn main() {
+    // Look up a color by name
+    let azure = color("Azure").unwrap();
+    
+    // Access color properties
+    println!("Name: {}", azure.name());     // "Azure"
+    println!("Hex: {}", azure.hex());       // "#007FFF"
+    println!("RGB: {:?}", azure.rgb());     // (0, 127, 255)
+    
+    // Forgiving lookups - these all return the same color
+    assert_eq!(color("Azure"), color("azure"));
+    assert_eq!(color("Azure"), color("AZURE"));
+    assert_eq!(color("Azure"), color("a z u r e"));
+    assert_eq!(color("Azure"), color("a-z-u-r-e"));
+}
---
+++ examples/owo_integration.rs
@@ -0,0 +1,38 @@
+#[cfg(feature = "owo")]
+use owo_colors::OwoColorize;
+#[cfg(feature = "owo")]
+use pigment::color;
+
+fn main() {
+    #[cfg(feature = "owo")]
+    {
+        println!("This example requires the 'owo' feature to be enabled.");
+        println!("Run with: cargo run --example owo_integration --features owo\n");
+
+        let azure = color("Azure").unwrap();
+
+        // Convert to owo-colors Rgb
+        let owo_color: owo_colors::Rgb = azure.into();
+
+        // Use with owo-colors
+        println!("{}", "Azure colored text".color(owo_color));
+
+        // Try a few more colors
+        let colors = [
+            "Crimson", "ForestGreen", "DeepSkyBlue", "Gold", "Purple"
+        ];
+
+        for color_name in colors {
+            if let Some(c) = color(color_name) {
+                let owo_color: owo_colors::Rgb = c.into();
+                println!("{}", color_name.color(owo_color));
+            }
+        }
+    }
+
+    #[cfg(not(feature = "owo"))]
+    {
+        println!("This example requires the 'owo' feature to be enabled.");
+        println!("Run with: cargo run --example owo_integration --features owo");
+    }
+}
